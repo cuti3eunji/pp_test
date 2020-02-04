5ef14b4fd66db30df54629620d5c02b12fd06bdd
Metabiz0350 <Metabiz0350@DESKTOP-KJJ1S4B>
2020-02-04 18:57:08 +0900

    qweqwewe

--- src/print/ImagePrint.java
+++ src/print/ImagePrint.java
@@ -7,7 +7,7 @@ public class ImagePrint {
 	protected Image printImage;
 
 	public static void main(String[] args) {
-		ImagePrint ip = new ImagePrint(System.getProperty("user.dir") + "\\src\\barcodeimg\\08682f64-620020955.gif");
+		ImagePrint ip = new ImagePrint(		System.getProperty("user.dir") + "\\src\\barcodeimg\\08682f64-620020955.gif");
 		System.exit(0);
 	}
 	public ImagePrint(String fileName) {
@@ -16,9 +16,9 @@ public class ImagePrint {
 
 		// 출력될 영역 설정
 		p.setImageableArea(1 * 72, // Left margin 1 inch //for mm 0.0395
-						1.5 * 72, // Top margin 1.5 inches
-						6.5 * 72, // Width 6.5 inches
-						8 * 72); // Height 8 inches
+				1.5 * 72, // Top margin 1.5 inches
+				6.5 * 72, // Width 6.5 inches
+				8 * 72); // Height 8 inches
 		PageFormat format = new PageFormat();
 		format.setPaper(p); // 페이지포맷 페이지영역설정을 인자로..
 		PrinterJob pj = PrinterJob.getPrinterJob();
